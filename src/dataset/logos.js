6a6f053d3af367f405965bc8b5d253219baee72e
MichaelOgundero <ogunderomichael@gmail.com>
2022-01-26 20:34:27 -0600

    added to the illustrations sections of the page

--- src/dataset/logos.js
+++ src/dataset/logos.js
@@ -1,10 +1,23 @@
 import exampleImage from "./../assets/images/testImage.jpg";
 import exampleImageDesktop from "./../assets/images/construction.jpg";
 
+import LogoOneA from "./../assets/images/desktop/Logos/LogoOne_A.jpg";
+import LogoOneB from "./../assets/images/desktop/Logos/LogoOne_B.jpg";
+import LogoOneC from "./../assets/images/desktop/Logos/LogoOne_C.jpg";
+import LogoOneD from "./../assets/images/desktop/Logos/LogoOne_D.jpg";
+
+import LogoTwoA from "./../assets/images/desktop/Logos/LogoTwo_A.jpg";
+import LogoTwoB from "./../assets/images/desktop/Logos/LogoTwo_B.jpg";
+import LogoTwoC from "./../assets/images/desktop/Logos/LogoTwo_C.jpg";
+import LogoTwoD from "./../assets/images/desktop/Logos/LogoTwo_D.jpg";
+
+import LogoThree from "./../assets/images/desktop/Logos/LogoThree.jpg";
+
 let logos = [
   {
-    image: exampleImage,
-    imageDesktop: exampleImageDesktop,
+    image: LogoOneA,
+    imageDesktop: LogoOneA,
+    additionalImages: [LogoOneB, LogoOneC, LogoOneD],
     attributes: [
       {
         attribute: "Software",
@@ -29,8 +42,9 @@ let logos = [
     ],
   },
   {
-    image: exampleImage,
-    imageDesktop: exampleImageDesktop,
+    image: LogoTwoA,
+    imageDesktop: LogoTwoA,
+    additionalImages: [LogoTwoB, LogoTwoC, LogoTwoD],
     attributes: [
       {
         attribute: "Software",
@@ -55,8 +69,8 @@ let logos = [
     ],
   },
   {
-    image: exampleImage,
-    imageDesktop: exampleImageDesktop,
+    image: LogoThree,
+    imageDesktop: LogoThree,
     attributes: [
       {
         attribute: "Software",
